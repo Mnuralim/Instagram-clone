117534b4bf5345bc0f7ba237c019f4d8e07966be
mnuralim <shadownur345.com>
2023-08-09 17:25:31 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    API_SECRET: "http://localhost:5000",
-    NEXTAUTH_SECRET: "djnsjbfjbfjsnfnsjfn877dhhd",
-  },
   images: {
     domains: ["res.cloudinary.com"],
   },
