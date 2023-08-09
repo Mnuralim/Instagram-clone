581cebbaefc6ad602a0923391c3ec3daa68646c1
mnuralim <shadownur345.com>
2023-08-09 17:53:09 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    API_SECRET: process.env.API_SECRET,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  },
   images: {
     domains: ["res.cloudinary.com"],
   },
