bef816eb0d9fe1de66ed37ba0231c4d6efd35c9b
mnuralim <shadownur345.com>
2023-08-12 22:58:17 +0800

    update

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className="flex relative items-center justify-center top-0 z-[9999999999999999999999999999999]">
+    <div className="flex relative items-center justify-center top-0 z-[9999999999999999999999999999999] bg-black min-h-screen">
       <div className="w-full h-[2px] bg-gradient-to-r from-[#FECD00] via-[#F9373F] to-[#C913B9] animate-slide-right absolute top-1 duration-700"></div>
     </div>
   );
